056bdb396def1f6255defe3e0c32dc796e1f5afb
Vyach Mayorskiy <vmayorskiy@acrotron.com>
2025-10-05 13:43:48 +0000

    Environments separated into dev, prod - ayechat.ai

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd());
+  const defineEnv = {
+    'import.meta.env': JSON.stringify({
+      ...env,
+      MODE: mode,
+    }),
+  };
+
+  //define: {
+  //  'process.env': env
+  //},
+  //
+  return {
+
+  define: defineEnv,
+
   plugins: [react()],
   server: {
     host: '0.0.0.0',                // accept connections from any IP
@@ -21,6 +38,7 @@ export default defineConfig({
       'localhost',
       '127.0.0.1',
       '.acrotron.com',         // optional wildcard
+      '.ayechat.ai',         // optional wildcard
       /.*\.ngrok\.io/          // allow ngrok tunnels
     ],
     // --------------------------------------------------------------
@@ -32,4 +50,5 @@ export default defineConfig({
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     },
   },
+  };
 })
