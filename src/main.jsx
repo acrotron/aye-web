0087a370d26f225b80ad6ed4be65cbaf2a3c7f33
Vyach Mayorskiy <vmayorskiy@acrotron.com>
2025-10-22 22:51:18 +0000

    Integrating web site telemetrics

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,22 @@ import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(awsconfig);
 
+// Apply Umami telemetrics
+const head = document.getElementsByTagName('head')[0];
+if (head) {
+  const analyticsId = import.meta.env.VITE_ANALYTICS_ID || 'NONE';
+  if (analyticsId !== 'NONE') {
+    const script = document.createElement('script');
+    script.src = 'https://cloud.umami.is/script.js';
+    script.defer = true;
+    script.setAttribute('data-website-id', analyticsId);
+    head.appendChild(script);
+  }
+} else {
+  console.log("Head element NOT found");
+}
+
+// main
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
@@ -19,4 +35,3 @@ createRoot(document.getElementById('root')).render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
